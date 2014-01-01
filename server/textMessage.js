4ec2a1d791a3005bab6960ee1ee19987e392d4b5
Amir Rajan <ar@amirrajan.net>
2014-01-01 10:29:24 -0600

    need to fix that -_-

--- server/textMessage.js
+++ server/textMessage.js
@@ -3,14 +3,12 @@ var client = require('twilio')(env.twilioAccountSid, env.twilioAuthToken);
 
 function send(message) {
   console.log(message);
-  /*
   client.sendSms({
     to: env.mobile,
     from: env.twilioAssignedPhoneNumber,
     body: message
   }, function(err, responseData) { 
   });
-  */
 }
 
 module.exports.send = send;
